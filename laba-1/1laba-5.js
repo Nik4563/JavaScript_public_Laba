04fc81de15a3a27b63c859a574fa899822e97b31
Nikita <104521386+Nik4563@users.noreply.github.com>
2025-10-03 17:37:08 +0300

    Add files via upload

---
+++ laba-1/1laba-5.js
@@ -0,0 +1,5 @@
+let sideLength = prompt("Введите длину стороны квадрата:");
+
+let area = parseFloat(sideLength) * parseFloat(sideLength);
+
+alert("Площадь квадрата со стороной " + sideLength + " равна: " + area);
